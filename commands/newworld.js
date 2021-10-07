72bc10268329c9683da48ac6c086aa58d6ae59b6
selim <selim@cartegriseminute.fr>
2021-10-07 18:06:35 +0200

    fix

--- commands/newworld.js
+++ commands/newworld.js
@@ -19,7 +19,9 @@ module.exports = {
      */
     run: async (client, message, args, { GuildDB }) => {
         console.log('Kaloon')
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({headless: true,
+            args: ['--no-sandbox','--disable-setuid-sandbox']
+        });
         const page = await browser.newPage();
         page.setUserAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36");
 
